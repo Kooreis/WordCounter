3a0d3edac43b2b5d31d2c912711161567e787f5e
Kooreis <kooreis.official@gmail.com>
2022-03-11 06:00:00 -0600

    In this update, the necessary libraries are imported and the main program class is defined. The user is prompted to enter a sentence, which is then read and stored in a variable.

--- Word_Counter.cs
+++ Word_Counter.cs
@@ -1,4 +1,3 @@
-```CSharp
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,27 +8,5 @@ class Program
     {
         Console.WriteLine("Enter a sentence:");
         string sentence = Console.ReadLine();
-        Dictionary<string, int> wordCount = new Dictionary<string, int>();
-        string[] words = sentence.Split(' ');
-
-        foreach (string word in words)
-        {
-            if (wordCount.ContainsKey(word))
-            {
-                wordCount[word]++;
-            }
-            else
-            {
-                wordCount[word] = 1;
-            }
-        }
-
-        foreach (KeyValuePair<string, int> pair in wordCount)
-        {
-            Console.WriteLine("Word: {0}, Count: {1}", pair.Key, pair.Value);
-        }
-
-        Console.ReadKey();
     }
 }
-```
