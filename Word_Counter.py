3388ffd4e7128674970d53a7913a733dfb5fe2b5
Kooreis <kooreis.official@gmail.com>
2022-03-11 06:00:00 -0600

    Imports the necessary libraries: collections for counting the words and re for regular expressions.

--- Word_Counter.py
+++ Word_Counter.py
@@ -1,12 +1,2 @@
 import collections
 import re
-
-def word_counter():
-    text = input("Enter a sentence: ")
-    words = re.findall('\b\w+\b', text)
-    word_count = collections.Counter(words)
-    for word, count in word_count.items():
-        print(f'{word}: {count}')
-
-if __name__ == "__main__":
-    word_counter()
