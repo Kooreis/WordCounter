0ff56be6667cb1c643da42e26bbae56e15f8030b
Kooreis <kooreis.official@gmail.com>
2022-03-12 07:00:00 -0600

    In this update, a new dictionary is created to store the words from the user's sentence and their corresponding counts. The sentence is split into individual words and stored in an array.

--- Word_Counter.cs
+++ Word_Counter.cs
@@ -1,12 +1,2 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Enter a sentence:");
-        string sentence = Console.ReadLine();
-    }
-}
+Dictionary<string, int> wordCount = new Dictionary<string, int>();
+string[] words = sentence.Split(' ');
