47de1cb784edf9fb8882927f74b10d4c2ca9e604
Kooreis <kooreis.official@gmail.com>
2022-03-12 07:00:00 -0600

    Define the main class and method, and set up a scanner to read user input from the console.

--- Word_Counter.java
+++ Word_Counter.java
@@ -1,3 +1,5 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+public class WordCounter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String sentence = scanner.nextLine();
